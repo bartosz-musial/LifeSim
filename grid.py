42f85bc305c8a6d42a0af9bf7bbb19681385d644
Bartosz Musial <bmusial@protonmail.com>
2024-12-26 03:21:28 +0100

    Added organism movement functionality

--- grid.py
+++ grid.py
@@ -25,9 +25,24 @@ class Grid:
                 self.grid[y][x] = "\033[32mF\033[0m"
                 break
 
-    def add_organism(self) -> None:
+    def add_organism(self) -> tuple:
         while True:
             x, y = self.generate_location()
             if self.is_occupied(x, y):
                 self.grid[y][x] = "\033[31mO\033[0m"
+                return x, y
+
+    def organism_movement(self, organism) -> None:
+        movement = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        while True:
+            dx, dy = random.choice(movement)
+            location = [organism.x, organism.y]
+            temp = [organism.x, organism.y]
+            temp[0] += dx
+            temp[1] += dy
+            if temp[0] > 0 and temp[1] > 0:
+                organism.x, organism.y = temp[0], temp[1]
+                organism.energy -= 1
+                self.grid[location[1]][location[0]] = "."
+                self.grid[temp[1]][temp[0]] = "\033[31mO\033[0m"
                 break
